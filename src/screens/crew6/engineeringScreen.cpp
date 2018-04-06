58bb8b755fa5480481cf63437c887dd2c6f6856b
Thomas <t.delclite@gmail.com>
2018-04-06 10:01:45 +0200

    Update engineeringScreen.cpp

--- src/screens/crew6/engineeringScreen.cpp
+++ src/screens/crew6/engineeringScreen.cpp
@@ -38,7 +38,7 @@ EngineeringScreen::EngineeringScreen(GuiContainer* owner)
     oxygen_display = new GuiKeyValueDisplay(this, "OXYGEN_DISPLAY", 0.45, "Oxygen", "");
     oxygen_display->setIcon("gui/icons/weapon-hvli")->setTextSize(20)->setPosition(20, 260, ATopLeft)->setSize(240, 40);
     
-    (new GuiSelfDestructButton(this, "SELF_DESTRUCT"))->setPosition(20, 300, ATopLeft)->setSize(240, 100);
+    //(new GuiSelfDestructButton(this, "SELF_DESTRUCT"))->setPosition(20, 300, ATopLeft)->setSize(240, 100);
     
     GuiElement* system_config_container = new GuiElement(this, "");
     system_config_container->setPosition(0, -50, ABottomCenter)->setSize(750 + 300, GuiElement::GuiSizeMax);
