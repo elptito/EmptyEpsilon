96053384b95a84b1ad8b323762f7e8e97acca2f3
Thomas <t.delclite@gmail.com>
2018-05-14 14:06:21 +0200

    Add the Callsign of the comms target and clear the log
    Only in the GM mode

--- src/spaceObjects/playerSpaceship.cpp
+++ src/spaceObjects/playerSpaceship.cpp
@@ -1185,6 +1185,8 @@ void PlayerSpaceship::onReceiveClientCommand(int32_t client_id, sf::Packet& pack
                 P<PlayerSpaceship> player = comms_target;
                 comms_state = CS_OpeningChannel;
                 comms_open_delay = comms_channel_open_time;
+				comms_target_name = comms_target->getCallSign();
+				comms_incomming_message = "Opened comms with " + comms_target->getCallSign();		
                 addToShipLog("Hailing: " + comms_target->getCallSign(), colorConfig.log_generic);
             }else{
                 comms_state = CS_Inactive;
