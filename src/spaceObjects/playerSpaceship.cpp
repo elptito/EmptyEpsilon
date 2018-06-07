1e5306401b902f27f8a2e2193a164a8d77fa6a25
Thomas <t.delclite@gmail.com>
2018-06-07 18:09:31 +0200

    test pb comms

--- src/spaceObjects/playerSpaceship.cpp
+++ src/spaceObjects/playerSpaceship.cpp
@@ -1238,8 +1238,13 @@ void PlayerSpaceship::onReceiveClientCommand(int32_t client_id, sf::Packet& pack
                 P<PlayerSpaceship> player = comms_target;
                 comms_state = CS_OpeningChannel;
                 comms_open_delay = comms_channel_open_time;
-                comms_incomming_message = "Opened comms with " + comms_target->getCallSign();
-                addToShipLog("Hailing: " + comms_target->getCallSign(), colorConfig.log_generic);
+                if (comms_target->getCallSign()){
+                    comms_incomming_message = "Opened comms with " + comms_target->getCallSign();
+                    addToShipLog("Hailing: " + comms_target->getCallSign(), colorConfig.log_generic);
+                }else{
+                    comms_incomming_message = "Opened comms with " + comms_target_name;
+                    addToShipLog("Hailing: " + comms_target_name, colorConfig.log_generic);
+                }
             }else{
                 comms_state = CS_Inactive;
             }
