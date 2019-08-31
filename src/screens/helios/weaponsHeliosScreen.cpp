c45caa24ee2fe898ce73961d73957444e1e97401
Amir Arad <greenshade@gmail.com>
2019-09-01 02:07:19 +0300

    WIP weapons helios screen

--- src/screens/helios/weaponsHeliosScreen.cpp
+++ src/screens/helios/weaponsHeliosScreen.cpp
@@ -2,8 +2,6 @@
 #include "gameGlobalInfo.h"
 #include "weaponsHeliosScreen.h"
 
-#include "screenComponents/missileTubeControls.h"
-#include "screenComponents/aimLock.h"
 #include "screenComponents/beamFrequencySelector.h"
 #include "screenComponents/beamTargetSelector.h"
 #include "screenComponents/powerDamageIndicator.h"
@@ -11,19 +9,22 @@
 #include "screenComponents/shieldsEnableButton.h"
 #include "screenComponents/alertOverlay.h"
 #include "screenComponents/customShipFunctions.h"
+#include "screenComponents/systemStatus.h"
 
 #include "gui/gui2_rotationdial.h"
 #include "gui/gui2_label.h"
 #include "gui/gui2_keyvaluedisplay.h"
+#include "gui/gui2_progressbar.h"
 
+sf::Color grey(128, 128, 128);
 WeaponsHeliosScreen::WeaponsHeliosScreen(GuiContainer* owner)
-: GuiOverlay(owner, "WEAPONS_SCREEN", colorConfig.background)
+: GuiOverlay(owner, "WEAPONS_SCREEN", colorConfig.background), load_type(MW_None), manual_aim(false), missile_target_angle(0)
 {
     // Render the radar shadow and background decorations.
-    background_gradient = new GuiOverlay(this, "BACKGROUND_GRADIENT", sf::Color::White);
+    GuiOverlay* background_gradient = new GuiOverlay(this, "BACKGROUND_GRADIENT", sf::Color::White);
     background_gradient->setTextureCenter("gui/BackgroundGradient");
 
-    background_crosses = new GuiOverlay(this, "BACKGROUND_CROSSES", sf::Color::White);
+    GuiOverlay* background_crosses = new GuiOverlay(this, "BACKGROUND_CROSSES", sf::Color::White);
     background_crosses->setTextureTiled("gui/BackgroundCrosses");
 
     // Render the alert level color overlay.
@@ -31,54 +32,57 @@ WeaponsHeliosScreen::WeaponsHeliosScreen(GuiContainer* owner)
 
     radar = new GuiRadarView(this, "HELMS_RADAR", PlayerSpaceship::weapons_radar_range, &targets, my_spaceship);
     radar->setPosition(0, 0, ACenter)->setSize(GuiElement::GuiSizeMatchHeight, 800);
-    radar->setRangeIndicatorStepSize(1000.0)->shortRange()->enableCallsigns()->enableHeadingIndicators()->setStyle(GuiRadarView::Circular);
-    radar->setCallbacks(
-        [this](sf::Vector2f position) {
-            targets.setToClosestTo(position, 250, TargetsContainer::Targetable);
-            if (my_spaceship && targets.get())
-                my_spaceship->commandSetTarget(targets.get());
-            else if (my_spaceship)
-                my_spaceship->commandSetTarget(NULL);
-        }, nullptr, nullptr
-    );
-    missile_aim = new GuiRotationDial(this, "MISSILE_AIM", -90, 360 - 90, 0, [this](float value){
-        tube_controls->setMissileTargetAngle(value);
-    });
+    radar->setRangeIndicatorStepSize(1000.0)->shortRange()->enableGhostDots()->enableWaypoints()->enableCallsigns()->enableHeadingIndicators()->setStyle(GuiRadarView::Circular);
+    radar->setFogOfWarStyle(GuiRadarView::RadarRangeAndLineOfSight)->setAutoOrient(true)->setShowSectors(false)->enableTargetProjections(this);
+
+    missile_aim = new GuiRotationDial(this, "MISSILE_AIM", 0, 360, 0, nullptr);
     missile_aim->setPosition(0, 0, ACenter)->setSize(GuiElement::GuiSizeMatchHeight, 850);
+    
+    energy_display = new GuiKeyValueDisplay(this, "ENERGY_DISPLAY", 0.45, "Energy", "");
+    energy_display->setIcon("gui/icons/energy")->setTextSize(20)->setPosition(20, 20, ATopLeft)->setSize(240, 40);
+    front_shield_display = new GuiKeyValueDisplay(this, "FRONT_SHIELD_DISPLAY", 0.45, "Front", "");
+    front_shield_display->setIcon("gui/icons/shields-fore")->setTextSize(20)->setPosition(20, 60, ATopLeft)->setSize(240, 40);
+    rear_shield_display = new GuiKeyValueDisplay(this, "REAR_SHIELD_DISPLAY", 0.45, "Rear", "");
+    rear_shield_display->setIcon("gui/icons/shields-aft")->setTextSize(20)->setPosition(20, 100, ATopLeft)->setSize(240, 40);
 
-    tube_controls = new GuiMissileTubeControls(this, "MISSILE_TUBES", my_spaceship);
-    tube_controls->setPosition(20, -20, ABottomLeft);
-    radar->enableTargetProjections(tube_controls);
+    (new GuiSystemStatus(this, "FRONT_STATUS", SYS_FrontShield, my_spaceship))->setPosition(-20, 20, ATopRight)->setSize(350, 40);
+    (new GuiSystemStatus(this, "REAR_STATUS", SYS_RearShield, my_spaceship))->setPosition(-20, 60, ATopRight)->setSize(350, 40);
+    (new GuiSystemStatus(this, "BEAMS_STATUS", SYS_BeamWeapons, my_spaceship))->setPosition(-20, 100, ATopRight)->setSize(350, 40);
+    (new GuiSystemStatus(this, "MISSILES_STATUS", SYS_MissileSystem, my_spaceship))->setPosition(-20, 140, ATopRight)->setSize(350, 40);
 
-    lock_aim = new AimLockButton(this, "LOCK_AIM", tube_controls, missile_aim, my_spaceship);
-    lock_aim->setPosition(250, 20, ATopCenter)->setSize(130, 50);
+    GuiAutoLayout* missileLoadLayout = new GuiAutoLayout(this, "MISSILE_LOAD_TYPES", GuiAutoLayout::LayoutVerticalTopToBottom);
+    missileLoadLayout->setPosition(20, 180, ATopLeft)->setSize(200, 40 * MW_Count);
 
-    if (gameGlobalInfo->use_beam_shield_frequencies || gameGlobalInfo->use_system_damage)
-    {
-        GuiElement* beam_info_box = new GuiElement(this, "BEAM_INFO_BOX");
-        beam_info_box->setPosition(-20, -120, ABottomRight)->setSize(280, 150);
-        (new GuiLabel(beam_info_box, "BEAM_INFO_LABEL", "Beam info", 30))->addBackground()->setSize(GuiElement::GuiSizeMax, 50);
-        (new GuiPowerDamageIndicator(beam_info_box, "", SYS_BeamWeapons, ACenterLeft, my_spaceship))->setSize(GuiElement::GuiSizeMax, 50);
-        (new GuiBeamFrequencySelector(beam_info_box, "BEAM_FREQUENCY_SELECTOR"))->setPosition(0, 0, ABottomRight)->setSize(GuiElement::GuiSizeMax, 50);
-        (new GuiBeamTargetSelector(beam_info_box, "BEAM_TARGET_SELECTOR"))->setPosition(0, -50, ABottomRight)->setSize(GuiElement::GuiSizeMax, 50);
+    for (int n = 0; n < MW_Count; n++)
+    {   
+        load_type_rows[n] = new GuiKeyValueDisplay(missileLoadLayout, "MISSILE_LOAD_" + string(n), 0.45, getMissileWeaponName(EMissileWeapons(n)), "");
+        load_type_rows[n]->setTextSize(20)->setSize(GuiElement::GuiSizeMax, 40);
+    }
+    load_type_rows[MW_Homing]->setIcon("gui/icons/weapon-homing.png");
+    load_type_rows[MW_Mine]->setIcon("gui/icons/weapon-mine.png");
+    load_type_rows[MW_EMP]->setIcon("gui/icons/weapon-emp.png");
+    load_type_rows[MW_Nuke]->setIcon("gui/icons/weapon-nuke.png");
+    load_type_rows[MW_HVLI]->setIcon("gui/icons/weapon-hvli.png");
 
-        if (!gameGlobalInfo->use_beam_shield_frequencies)
-        {   //If we do have system damage, but no shield frequencies, we can partially overlap this with the shield button.
-            //So move the beam configuration a bit down.
-            beam_info_box->setPosition(-20, -50, ABottomRight);
-        }
+    GuiAutoLayout* tubesLayout = new GuiAutoLayout(this, "TUBES", GuiAutoLayout::LayoutVerticalBottomToTop);
+    tubesLayout->setPosition(20, -20, ABottomLeft)->setSize(200, 50 * max_weapon_tubes);
+
+    for (int n = max_weapon_tubes - 1; n >= 0; n--) {
+        tube_rows[n] = new GuiProgressbar(tubesLayout, "TUBE_" + string(n), 0, 1.0, 0);
+        tube_rows[n]->setSize(GuiElement::GuiSizeMax, 50);
     }
 
-    energy_display = new GuiKeyValueDisplay(this, "ENERGY_DISPLAY", 0.45, "Energy", "");
-    energy_display->setIcon("gui/icons/energy")->setTextSize(20)->setPosition(20, 100, ATopLeft)->setSize(240, 40);
-    front_shield_display = new GuiKeyValueDisplay(this, "FRONT_SHIELD_DISPLAY", 0.45, "Front", "");
-    front_shield_display->setIcon("gui/icons/shields-fore")->setTextSize(20)->setPosition(20, 140, ATopLeft)->setSize(240, 40);
-    rear_shield_display = new GuiKeyValueDisplay(this, "REAR_SHIELD_DISPLAY", 0.45, "Rear", "");
-    rear_shield_display->setIcon("gui/icons/shields-aft")->setTextSize(20)->setPosition(20, 180, ATopLeft)->setSize(240, 40);
+    if (gameGlobalInfo->use_beam_shield_frequencies || gameGlobalInfo->use_system_damage) {
+        beams_display = new GuiLabel(this, "BEAMS_DISPLAY", "Beams", 20);
+        beams_display->setBold()->addBackground()->setPosition(-20, 180, ATopRight)->setSize(350, 40);
+    }
 
-    if (gameGlobalInfo->use_beam_shield_frequencies)
-    {
+    if (gameGlobalInfo->use_beam_shield_frequencies) {
         //The shield frequency selection includes a shield enable button.
+        // TODO: 
+        // shields_display = new GuiProgressbar()
+        // new_shields_frequency_display = GuiKeyValueDisplay()
+
         (new GuiShieldFrequencySelect(this, "SHIELD_FREQ"))->setPosition(-20, -20, ABottomRight)->setSize(280, 100);
     }else{
         (new GuiShieldsEnableButton(this, "SHIELDS_ENABLE", my_spaceship))->setPosition(-20, -20, ABottomRight)->setSize(280, 50);
@@ -94,20 +98,58 @@ void WeaponsHeliosScreen::onDraw(sf::RenderTarget& window)
         energy_display->setValue(string(int(my_spaceship->energy_level)));
         front_shield_display->setValue(string(my_spaceship->getShieldPercentage(0)) + "%");
         rear_shield_display->setValue(string(my_spaceship->getShieldPercentage(1)) + "%");
+        string beamsSystemName = my_spaceship->beam_system_target == SYS_None? "Hull" : getSystemName(ESystem(my_spaceship->beam_system_target));
+        beams_display->setText(frequencyToString(my_spaceship->beam_frequency) + " Beams: " + beamsSystemName);
         targets.set(my_spaceship->getTarget());
+        for (int n = 0; n < MW_Count; n++)
+        {
+            sf::Color color = n == load_type? sf::Color::Yellow :  sf::Color::White ;
+            if (my_spaceship->weapon_storage[n] == 0){
+                color *= grey;
+            }
+            load_type_rows[n]->setValue(" [" + string(my_spaceship->weapon_storage[n]) + "/" + string(my_spaceship->weapon_storage_max[n]) + "]");
+            load_type_rows[n]->setVisible(my_spaceship->weapon_storage_max[n] > 0);
+            load_type_rows[n]->setColor(color)->setContentColor(color);
+        }
 
-        lock_aim->setVisible(my_spaceship->ship_template->has_manual_aim);
-        missile_aim->setVisible(tube_controls->getManualAim() && my_spaceship->ship_template->has_manual_aim);
+        for (int n = 0; n < my_spaceship->weapon_tube_count; n++) {
+            WeaponTube& tube = my_spaceship->weapon_tube[n];
+            tube_rows[n]->show();
+            
+            if(tube.isEmpty()) {
+                tube_rows[n]->setText(tube.getTubeName() + ": Empty");
+                tube_rows[n]->setValue(0.f);
+                tube_rows[n]->setTextColor(tube.canLoad(load_type)? sf::Color::Yellow : sf::Color::White);
+            } else if(tube.isLoaded()) {
+                tube_rows[n]->setText(tube.getTubeName() + ": " + getMissileWeaponName(tube.getLoadType()));
+                tube_rows[n]->setValue(1.0f);
+                tube_rows[n]->setColor(sf::Color::Green)->setTextColor(sf::Color::Black);
+            } else if(tube.isLoading()) {
+                tube_rows[n]->setText(tube.getTubeName() + ": " + getMissileWeaponName(tube.getLoadType()));
+                tube_rows[n]->setValue(tube.getLoadProgress());
+                tube_rows[n]->setColor(sf::Color::Yellow)->setTextColor(grey);
+            } else if(tube.isUnloading()) {
+                tube_rows[n]->setText(getMissileWeaponName(tube.getLoadType()));
+                tube_rows[n]->setValue(1.f - tube.getLoadProgress());
+                tube_rows[n]->setColor(grey)->setTextColor(sf::Color::White);
+            } else if(tube.isFiring()) {
+                tube_rows[n]->setText("Firing");
+                tube_rows[n]->setValue(0.f);
+                tube_rows[n]->setColor(grey)->setTextColor(sf::Color::White);
+            }
+        }
+        for(int n=my_spaceship->weapon_tube_count; n<max_weapon_tubes; n++)
+            tube_rows[n]->hide();
+        missile_aim->setValue(missile_target_angle - my_spaceship->getRotation());
     }
     GuiOverlay::onDraw(window);
 }
 
+//         targets.setToClosestTo(position, 250, TargetsContainer::Targetable);
 void WeaponsHeliosScreen::onHotkey(const HotkeyResult& key)
 {
-    if (key.category == "WEAPONS" && my_spaceship)
-    {
-        if (key.hotkey == "NEXT_ENEMY_TARGET")
-        {
+    if (key.category == "WEAPONS" && my_spaceship) {
+        if (key.hotkey == "NEXT_ENEMY_TARGET") {
             bool current_found = false;
             foreach(SpaceObject, obj, space_object_list)
             {
@@ -136,9 +178,7 @@ void WeaponsHeliosScreen::onHotkey(const HotkeyResult& key)
                     return;
                 }
             }
-        }
-        if (key.hotkey == "NEXT_TARGET")
-        {
+        } else if (key.hotkey == "NEXT_TARGET") {
             bool current_found = false;
             foreach(SpaceObject, obj, space_object_list)
             {
@@ -167,28 +207,92 @@ void WeaponsHeliosScreen::onHotkey(const HotkeyResult& key)
                     return;
                 }
             }
+        } else if (key.hotkey == "SELECT_MISSILE_TYPE_HOMING") {
+            load_type = MW_Homing;
+        } else if (key.hotkey == "SELECT_MISSILE_TYPE_NUKE") {
+            load_type = MW_Nuke;
+        } else if (key.hotkey == "SELECT_MISSILE_TYPE_MINE") {
+            load_type = MW_Mine;
+        } else if (key.hotkey == "SELECT_MISSILE_TYPE_EMP") {
+            load_type = MW_EMP;
+        } else if (key.hotkey == "SELECT_MISSILE_TYPE_HVLI") {
+            load_type = MW_HVLI;
+        } else if (key.hotkey == "BEAM_FREQUENCY_INCREASE") {
+            my_spaceship->commandSetBeamFrequency((my_spaceship->beam_frequency + 1) % SpaceShip::max_frequency);
+        } else if (key.hotkey == "BEAM_FREQUENCY_DECREASE") {
+            my_spaceship->commandSetBeamFrequency((SpaceShip::max_frequency + my_spaceship->beam_frequency - 1) % SpaceShip::max_frequency);
         }
-        if (my_spaceship->ship_template->has_manual_aim){
-            if (key.hotkey == "AIM_MISSILE_LEFT")
-            {
-                missile_aim->setValue(missile_aim->getValue() - 5.0f);
-                tube_controls->setMissileTargetAngle(missile_aim->getValue());
+        if (gameGlobalInfo->use_system_damage){
+            // const int targetSysCount = SYS_COUNT +1;
+            if (key.hotkey == "BEAM_SUBSYSTEM_TARGET_NEXT") {
+                // ESystem system = ESystem(((my_spaceship->beam_system_target +2) % targetSysCount) -1);
+                int system = my_spaceship->beam_system_target + 1;
+                if (system == SYS_JumpDrive && gameGlobalInfo->player_warp_jump_drive_setting == PWJ_WarpDrive) {
+                    system = ESystem(system + 1);
+                } else if (system >= SYS_COUNT) {
+                    system = -1;
+                }
+                my_spaceship->commandSetBeamSystemTarget(ESystem(system));
+            } else if (key.hotkey == "BEAM_SUBSYSTEM_TARGET_PREV") {
+                // ESystem system = ESystem(((targetSysCount + my_spaceship->beam_system_target) % targetSysCount) -1);
+                int system = my_spaceship->beam_system_target - 1;
+                if (system == SYS_JumpDrive && gameGlobalInfo->player_warp_jump_drive_setting == PWJ_WarpDrive) {
+                    system = ESystem(system - 1);
+                } else if (system <= -2) {
+                    system = SYS_COUNT -1;
+                }
+                my_spaceship->commandSetBeamSystemTarget(ESystem(system));
+            } 
+        }
+        if (my_spaceship->ship_template->has_manual_aim) {
+            if (key.hotkey == "AIM_MISSILE_LEFT") {
+                missile_target_angle = missile_target_angle - 5.0f;
+            } else if (key.hotkey == "AIM_MISSILE_RIGHT") {
+                missile_target_angle = missile_target_angle + 5.0f;
+            } else if (key.hotkey == "ENABLE_AIM_LOCK") {
+                manual_aim = false;
+            } else if (key.hotkey == "DISABLE_AIM_LOCK") {
+                manual_aim = true;
             }
-            if (key.hotkey == "AIM_MISSILE_RIGHT")
+        }
+        for(int n=0; n<my_spaceship->weapon_tube_count; n++)
+        {
+            WeaponTube& tube = my_spaceship->weapon_tube[n];
+            if (key.hotkey == "LOAD_TUBE_" + string(n+1) && tube.isEmpty())
+                my_spaceship->commandLoadTube(n, load_type);
+            if (key.hotkey == "UNLOAD_TUBE_" + string(n+1) && tube.isLoaded())
+                my_spaceship->commandUnloadTube(n);
+            if (key.hotkey == "FIRE_TUBE_" + string(n+1) && tube.isLoaded())
             {
-                missile_aim->setValue(missile_aim->getValue() + 5.0f);
-                tube_controls->setMissileTargetAngle(missile_aim->getValue());
+                float target_angle = missile_target_angle;
+                if (!manual_aim)
+                {
+                    target_angle = my_spaceship->weapon_tube[n].calculateFiringSolution(my_spaceship->getTarget());
+                    if (target_angle == std::numeric_limits<float>::infinity())
+                        target_angle = my_spaceship->getRotation() + my_spaceship->weapon_tube[n].getDirection();
+                }
+                my_spaceship->commandFireTube(n, target_angle);
             }
         }
     }
 }
+
 bool WeaponsHeliosScreen::onJoystickAxis(const AxisAction& axisAction){
     if (axisAction.category == "WEAPONS" && my_spaceship){
-        if (axisAction.action == "AIM_MISSILE" && my_spaceship->ship_template->has_manual_aim){
-            missile_aim->setValue(axisAction.value * 180);
-            tube_controls->setMissileTargetAngle(missile_aim->getValue());
+        if (axisAction.action == "AIM_MISSILE"){
+            missile_target_angle = axisAction.value * 180;
             return true;
         } 
     }
     return false;
+}
+
+float WeaponsHeliosScreen::getMissileTargetAngle()
+{
+    return missile_target_angle;
+}
+
+bool WeaponsHeliosScreen::getManualAim()
+{
+    return manual_aim;
 }
--- src/screens/helios/weaponsHeliosScreen.h
+++ src/screens/helios/weaponsHeliosScreen.h
@@ -4,33 +4,45 @@
 #include "gui/gui2_overlay.h"
 #include "screenComponents/radarView.h"
 #include "screenComponents/targetsContainer.h"
+#include "screenComponents/missileAim.h"
 #include "gui/joystickConfig.h"
 
-class GuiMissileTubeControls;
+// class GuiMissileTubeControls;
 class GuiKeyValueDisplay;
-class GuiToggleButton;
 class GuiRotationDial;
+class GuiProgressbar;
+class GuiLabel;
 
-class WeaponsHeliosScreen : public GuiOverlay
+class WeaponsHeliosScreen : public GuiOverlay, public MissileAim
 {
 private:
-    GuiOverlay* background_gradient;
-    GuiOverlay* background_crosses;
 
+    GuiRadarView* radar;
     TargetsContainer targets;
+    GuiRotationDial* missile_aim;
+
     GuiKeyValueDisplay* energy_display;
     GuiKeyValueDisplay* front_shield_display;
     GuiKeyValueDisplay* rear_shield_display;
-    GuiRadarView* radar;
-    GuiMissileTubeControls* tube_controls;
-    GuiRotationDial* missile_aim;
-    GuiToggleButton* lock_aim;
+    GuiLabel* beams_display;
+    GuiProgressbar* shields_display;
+    GuiKeyValueDisplay* new_shields_frequency_display;
+
+    GuiKeyValueDisplay* load_type_rows[MW_Count];
+    GuiProgressbar* tube_rows[max_weapon_tubes];
+
+    EMissileWeapons load_type;
+    bool manual_aim;
+    float missile_target_angle;
 public:
     WeaponsHeliosScreen(GuiContainer* owner);
     
     virtual void onDraw(sf::RenderTarget& window) override;
     virtual void onHotkey(const HotkeyResult& key) override;
     virtual bool onJoystickAxis(const AxisAction& axisAction) override;
+
+    virtual float getMissileTargetAngle() override;
+    virtual bool getManualAim() override;
 };
 
 #endif//WEAPONS_HELIOS_SCREEN_H
