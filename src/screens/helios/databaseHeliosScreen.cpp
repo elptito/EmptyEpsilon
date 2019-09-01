0d08246ae00912393eaf269f181543502b08a535
Amir Arad <greenshade@gmail.com>
2019-09-01 23:18:16 +0300

    db helios screen layout

--- src/screens/helios/databaseHeliosScreen.cpp
+++ src/screens/helios/databaseHeliosScreen.cpp
@@ -1,13 +1,114 @@
 #include "databaseHeliosScreen.h"
-#include "scienceDatabase.h"
-
-#include "screenComponents/databaseView.h"
 #include "screenComponents/customShipFunctions.h"
+#include "gui/gui2_element.h"
+#include "scienceDatabase.h"
+#include "gui/gui2_listbox.h"
+#include "gui/gui2_autolayout.h"
+#include "gui/gui2_keyvaluedisplay.h"
+#include "gui/gui2_scrolltext.h"
+#include "screenComponents/rotatingModelView.h"
 
+#define MARGIN 20
 DatabaseHeliosScreen::DatabaseHeliosScreen(GuiContainer* owner)
 : GuiOverlay(owner, "DATABASE_SCREEN", colorConfig.background)
 {
-    (new DatabaseViewComponent(this))->setPosition(0, 0, ATopLeft)->setSize(GuiElement::GuiSizeMax, GuiElement::GuiSizeMax);
+    database_entry = nullptr;
+
+    item_list = new GuiListbox(this, "DATABASE_ITEM_LIST", [this](int index, string value) {
+        P<ScienceDatabase> entry;
+        if (selected_entry) {
+            if (index == 0) {
+                selected_entry = selected_entry->parent;
+                fillListBox();
+            } else {
+                entry = selected_entry->items[index - 1];
+            }
+        } else {
+            entry = ScienceDatabase::science_databases[index];
+        }
+        display(entry);
+    });
+    item_list->setPosition(0, 0, ATopLeft)->setMargins(20, 20, 20, 130)->setSize(400, GuiElement::GuiSizeMax);
+    fillListBox();
 
     (new GuiCustomShipFunctions(this, databaseView, "", my_spaceship))->setPosition(-20, 120, ATopRight)->setSize(250, GuiElement::GuiSizeMax);
+}
+
+
+bool DatabaseHeliosScreen::findAndDisplayEntry(string name)
+{
+    foreach(ScienceDatabase, sd, ScienceDatabase::science_databases)
+    {
+        if (findAndDisplayEntry(name, sd))
+            return true;
+    }
+    return false;
+}
+
+bool DatabaseHeliosScreen::findAndDisplayEntry(string name, P<ScienceDatabase> parent)
+{
+    foreach(ScienceDatabase, sd, parent->items)
+    {
+        if (sd->getName() == name)
+        {
+            selected_entry = parent;
+            fillListBox();
+            display(sd);
+            item_list->setSelectionIndex(item_list->indexByValue(name));
+            return true;
+        }
+        if (findAndDisplayEntry(name, sd))
+            return true;
+    }
+    return false;
+}
+
+void DatabaseHeliosScreen::fillListBox()
+{
+    item_list->setOptions({});
+    item_list->setSelectionIndex(-1);
+    if (selected_entry) {
+        item_list->addEntry("Back", "");
+        foreach(ScienceDatabase, sd, selected_entry->items) {
+            item_list->addEntry(sd->getName(), sd->getName());
+        }
+    } else {
+        foreach(ScienceDatabase, sd, ScienceDatabase::science_databases) {
+            item_list->addEntry(sd->getName(), sd->getName());
+        }
+    }
+}
+
+void DatabaseHeliosScreen::display(P<ScienceDatabase> entry)
+{
+    if (database_entry)
+        database_entry->destroy();
+    
+    database_entry = new GuiAutoLayout(this, "DATABASE_ENTRY", GuiAutoLayout::LayoutVerticalColumns);
+    database_entry->setPosition(400, 20, ATopLeft)->setSize(GuiElement::GuiSizeMax, GuiElement::GuiSizeMax);
+    
+    if (!entry)
+        return;
+
+    if (entry->keyValuePairs.size() > 0) {
+        GuiAutoLayout* vLayout = new GuiAutoLayout(database_entry, "DATABASE_ENTRY_V_LAYOUT", GuiAutoLayout::LayoutVerticalTopToBottom);
+        vLayout->setMargins(MARGIN);
+
+        for(unsigned int n=0; n<entry->keyValuePairs.size(); n++) {
+            (new GuiKeyValueDisplay(vLayout, "", 0.37, entry->keyValuePairs[n].key, entry->keyValuePairs[n].value))->setSize(GuiElement::GuiSizeMax, 40);
+        }
+    }
+    if (entry->model_data || entry->longDescription.length() > 0){
+        GuiAutoLayout* hLayout = new GuiAutoLayout(database_entry, "DATABASE_ENTRY_H_LAYOUT", GuiAutoLayout::LayoutHorizontalRows);
+        if (entry->model_data) {
+            (new GuiRotatingModelView(hLayout, "DATABASE_MODEL_VIEW", entry->model_data))->setMargins(MARGIN);
+        }
+        if (entry->longDescription.length() > 0) {
+            (new GuiScrollText(hLayout, "DATABASE_LONG_DESCRIPTION", entry->longDescription))->setTextSize(24)->setMargins(MARGIN);
+        }
+    }
+    if (entry->items.size() > 0) {
+        selected_entry = entry;
+        fillListBox();
+    }
 }
--- src/screens/helios/databaseHeliosScreen.h
+++ src/screens/helios/databaseHeliosScreen.h
@@ -4,11 +4,24 @@
 #include "gui/gui2_overlay.h"
 #include "shipTemplate.h"
 
+class ScienceDatabase;
+class GuiListbox;
+class GuiElement;
+
 class DatabaseHeliosScreen : public GuiOverlay
 {
-private:
+    P<ScienceDatabase> selected_entry;
+    GuiListbox* item_list;
+    GuiElement* database_entry;
 public:
     DatabaseHeliosScreen(GuiContainer* owner);
+    bool findAndDisplayEntry(string name);
+private:
+    bool findAndDisplayEntry(string name, P<ScienceDatabase> parent);
+    //Fill the selection listbox with options from the selected_entry, or the main database list if selected_entry is nullptr
+    void fillListBox();
+    void display(P<ScienceDatabase> entry);
+
 };
 
 #endif//DATABASE_HELIOS_SCREEN_H
