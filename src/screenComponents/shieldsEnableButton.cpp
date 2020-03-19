d2482db4c72b0a662ed57fed2516b631b7b2ec24
aBlueShadow <jilg@arcor.de>
2020-03-19 16:34:50 +0100

    indentation fix #2

--- src/screenComponents/shieldsEnableButton.cpp
+++ src/screenComponents/shieldsEnableButton.cpp
@@ -53,9 +53,9 @@ void GuiShieldsEnableButton::onDraw(sf::RenderTarget& window)
             button->setValue(target_spaceship->shields_active);
             bar->hide();
             if (gameGlobalInfo->use_beam_shield_frequencies)
-			{
-				string shield_status=my_spaceship->shields_active ? tr("shields","ON") : tr("shields","OFF");
-				button->setText(tr("{frequency} Shields: {status}").format({{"frequency", frequencyToString(my_spaceship->shield_frequency)}, {"status", shield_status}}));
+	    {
+		string shield_status=my_spaceship->shields_active ? tr("shields","ON") : tr("shields","OFF");
+		button->setText(tr("{frequency} Shields: {status}").format({{"frequency", frequencyToString(my_spaceship->shield_frequency)}, {"status", shield_status}}));
             }
             else
                 button->setText(target_spaceship->shields_active ? " Bouclier: ON" : " Bouclier: OFF");
