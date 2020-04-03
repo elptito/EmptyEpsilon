fcc8d6850d805f7e65f88e737e8415aa5427dd14
Garrett Guillotte <garrett+ozgh@oznogon.com>
2020-04-02 21:16:22 -0700

    Set preference defaults

--- src/screens/mainScreen.cpp
+++ src/screens/mainScreen.cpp
@@ -91,7 +91,7 @@ ScreenMainScreen::ScreenMainScreen()
     rotatetime = 0.0007;
     angle = 0.0f;
     // Initialize and play the impulse engine sound.
-    impulse_sound = std::unique_ptr<ImpulseSound>( new ImpulseSound(PreferencesManager::get("impulse_sound_enabled") != "0") );
+    impulse_sound = std::unique_ptr<ImpulseSound>( new ImpulseSound(PreferencesManager::get("impulse_sound_enabled", "2") != "0") );
 
     first_person = PreferencesManager::get("first_person") == "1";
 }
