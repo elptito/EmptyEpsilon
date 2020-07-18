911743e8b3d917a5b4dec71792f8233653693194
Tom <63907376+capt-t@users.noreply.github.com>
2020-07-18 20:00:00 +0200

    comms_ship: tabs to spaces

--- scripts/comms_ship.lua
+++ scripts/comms_ship.lua
@@ -10,27 +10,27 @@ local MISSILE_TYPES = {"Homing", "Nuke", "Mine", "EMP", "HVLI"}
 --
 -- Uses one of `friendlyComms`, `neutralComms`, `enemyComms`.
 function mainMenu()
-	if comms_target.comms_data == nil then
-		comms_target.comms_data = {friendlyness = random(0.0, 100.0)}
-	end
-	-- comms_data is used globally
-	comms_data = comms_target.comms_data
-	
-	if player:isFriendly(comms_target) then
-		return friendlyComms(comms_data)
-	end
-	if player:isEnemy(comms_target) and comms_target:isFriendOrFoeIdentifiedBy(player) then
-		return enemyComms(comms_data)
-	end
-	return neutralComms(comms_data)
+    if comms_target.comms_data == nil then
+        comms_target.comms_data = {friendlyness = random(0.0, 100.0)}
+    end
+    -- comms_data is used globally
+    comms_data = comms_target.comms_data
+
+    if player:isFriendly(comms_target) then
+        return friendlyComms(comms_data)
+    end
+    if player:isEnemy(comms_target) and comms_target:isFriendOrFoeIdentifiedBy(player) then
+        return enemyComms(comms_data)
+    end
+    return neutralComms(comms_data)
 end
 
 --- Handle friendly communication.
 function friendlyComms(comms_data)
     if comms_data.friendlyness < 20 then
-        setCommsMessage("What do you want?")
+        setCommsMessage("What do you want?");
     else
-        setCommsMessage("Sir, how can we assist?")
+        setCommsMessage("Sir, how can we assist?");
     end
     addCommsReply("Defend a waypoint", function()
         if player:getWaypointCount() == 0 then
@@ -68,27 +68,27 @@ function friendlyComms(comms_data)
             end
         end
 
-		-- TODO this should use a global
-		local missile_types = {'Homing', 'Nuke', 'Mine', 'EMP', 'HVLI'}
-		for i, missile_type in ipairs(missile_types) do
-			if comms_target:getWeaponStorageMax(missile_type) > 0 then
-					msg = msg .. missile_type .. " Missiles: " .. math.floor(comms_target:getWeaponStorage(missile_type)) .. "/" .. math.floor(comms_target:getWeaponStorageMax(missile_type)) .. "\n"
-			end
-		end
+        -- TODO this should use a global
+        local missile_types = {'Homing', 'Nuke', 'Mine', 'EMP', 'HVLI'}
+        for i, missile_type in ipairs(missile_types) do
+            if comms_target:getWeaponStorageMax(missile_type) > 0 then
+                    msg = msg .. missile_type .. " Missiles: " .. math.floor(comms_target:getWeaponStorage(missile_type)) .. "/" .. math.floor(comms_target:getWeaponStorageMax(missile_type)) .. "\n"
+            end
+        end
 
-		setCommsMessage(msg);
-		addCommsReply("Back", mainMenu)
-	end)
-	for _, obj in ipairs(comms_target:getObjectsInRange(5000)) do
-		if obj.typeName == "SpaceStation" and not comms_target:isEnemy(obj) then
-			addCommsReply("Dock at " .. obj:getCallSign(), function()
-				setCommsMessage("Docking at " .. obj:getCallSign() .. ".");
-				comms_target:orderDock(obj)
-				addCommsReply("Back", mainMenu)
-			end)
-		end
-	end
-	return true
+        setCommsMessage(msg);
+        addCommsReply("Back", mainMenu)
+    end)
+    for _, obj in ipairs(comms_target:getObjectsInRange(5000)) do
+        if obj.typeName == "SpaceStation" and not comms_target:isEnemy(obj) then
+            addCommsReply("Dock at " .. obj:getCallSign(), function()
+                setCommsMessage("Docking at " .. obj:getCallSign() .. ".");
+                comms_target:orderDock(obj)
+                addCommsReply("Back", mainMenu)
+            end)
+        end
+    end
+    return true
 end
 
 --- Handle enemy communication.
@@ -99,23 +99,23 @@ function enemyComms(comms_data)
         local taunt_success_reply = "Your bloodline will end here!"
         local taunt_failed_reply = "Your feeble threats are meaningless."
         if faction == "Kraylor" then
-            setCommsMessage("Ktzzzsss.\nYou will DIEEee weaklingsss!")
+            setCommsMessage("Ktzzzsss.\nYou will DIEEee weaklingsss!");
         elseif faction == "Arlenians" then
-            setCommsMessage("We wish you no harm, but will harm you if we must.\nEnd of transmission.")
+            setCommsMessage("We wish you no harm, but will harm you if we must.\nEnd of transmission.");
         elseif faction == "Exuari" then
-            setCommsMessage("Stay out of our way, or your death will amuse us extremely!")
+            setCommsMessage("Stay out of our way, or your death will amuse us extremely!");
         elseif faction == "Ghosts" then
-            setCommsMessage("One zero one.\nNo binary communication detected.\nSwitching to universal speech.\nGenerating appropriate response for target from human language archives.\n:Do not cross us:\nCommunication halted.")
+            setCommsMessage("One zero one.\nNo binary communication detected.\nSwitching to universal speech.\nGenerating appropriate response for target from human language archives.\n:Do not cross us:\nCommunication halted.");
             taunt_option = "EXECUTE: SELFDESTRUCT"
             taunt_success_reply = "Rogue command received. Targeting source."
             taunt_failed_reply = "External command ignored."
         elseif faction == "Ktlitans" then
-            setCommsMessage("The hive suffers no threats. Opposition to any of us is opposition to us all.\nStand down or prepare to donate your corpses toward our nutrition.")
+            setCommsMessage("The hive suffers no threats. Opposition to any of us is opposition to us all.\nStand down or prepare to donate your corpses toward our nutrition.");
             taunt_option = "<Transmit 'The Itsy-Bitsy Spider' on all wavelengths>"
             taunt_success_reply = "We do not need permission to pluck apart such an insignificant threat."
             taunt_failed_reply = "The hive has greater priorities than exterminating pests."
         else
-            setCommsMessage("Mind your own business!")
+            setCommsMessage("Mind your own business!");
         end
         comms_data.friendlyness = comms_data.friendlyness - random(0, 10)
         addCommsReply(taunt_option, function()
@@ -134,9 +134,9 @@ end
 --- Handle neutral communiction.
 function neutralComms(comms_data)
     if comms_data.friendlyness > 50 then
-        setCommsMessage("Sorry, we have no time to chat with you.\nWe are on an important mission.")
+        setCommsMessage("Sorry, we have no time to chat with you.\nWe are on an important mission.");
     else
-        setCommsMessage("We have nothing for you.\nGood day.")
+        setCommsMessage("We have nothing for you.\nGood day.");
     end
     return true
 end
