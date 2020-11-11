cfe2e64da627a0f0811b6c6a0bac571f5189c048
Tdelc <t.delclite@gmail.com>
2020-11-11 21:52:55 +0100

    first french realases with point zero universe

--- src/main.cpp
+++ src/main.cpp
@@ -176,7 +176,7 @@ int main(int argc, char** argv)
     textureManager.setDefaultRepeated(true);
     textureManager.setAutoSprite(false);
     textureManager.getTexture("Tokka_WalkingMan.png", sf::Vector2i(6, 1)); //Setup the sprite mapping.
-    i18n::load("locale/" + PreferencesManager::get("language", "en") + ".po");
+    i18n::load("locale/" + PreferencesManager::get("language", "fr") + ".po");
 
     if (PreferencesManager::get("httpserver").toInt() != 0)
     {
