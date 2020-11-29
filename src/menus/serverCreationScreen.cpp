c801c085fcf4f5ab819185497a6feb4f74fb2d0a
tsht <tsht@gmx.fr>
2020-11-29 23:05:34 +0100

    Correction typo

--- src/menus/serverCreationScreen.cpp
+++ src/menus/serverCreationScreen.cpp
@@ -39,7 +39,7 @@ ServerCreationScreen::ServerCreationScreen()
     gameGlobalInfo->allow_main_screen_ship_state = PreferencesManager::get("server_config_allow_main_screen_ship_state", "1").toInt();
     gameGlobalInfo->use_nano_repair_crew = PreferencesManager::get("server_use_nano_repair_crew", "1").toInt();
     gameGlobalInfo->gm_control_code = PreferencesManager::get("server_config_gm_control_code", "").upper();
-    gameGlobalInfo->use_long_range_for_relay = PreferenceManager::get("server_use_long_range_for_relay", "1").toInt();
+    gameGlobalInfo->use_long_range_for_relay = PreferencesManager::get("server_use_long_range_for_relay", "1").toInt();
     gameGlobalInfo->use_complex_radar_signatures = PreferencesManager::get("server_config_use_complex_radar_signatures", "0").toInt();
 
     // Create a two-column layout.
